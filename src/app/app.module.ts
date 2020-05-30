6f14828d7fe2f4f558cc607bad91c8394af8eb36
Valoriel <kostyan001301@gmail.com>
2020-05-30 23:00:43 +0300

    added svg-icons and fixed normalize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularSvgIconModule } from 'angular-svg-icon';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, AngularSvgIconModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent],
 })
