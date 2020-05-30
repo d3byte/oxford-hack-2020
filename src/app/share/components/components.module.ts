e9c659c834007074bd81e3d73ee55403b2b84a1f
d3byte <pandinator@protonmail.com>
2020-05-30 21:30:38 +0300

    feat: reworked button component

--- src/app/share/components/components.module.ts
+++ src/app/share/components/components.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 
-import { CustomButtonModule } from '@share/components/custom-button/custom-button.module';
+import { ButtonModule } from '@share/components/button/button.module';
 import { TopicCardModule } from '@share/components/topic-card/topic-card.module';
 
 @NgModule({
-  exports: [TopicCardModule, CustomButtonModule],
+  exports: [TopicCardModule, ButtonModule],
 })
 export class ComponentsModule {}
--- src/app/share/interfaces/button.interface.ts
+++
@@ -1,3 +0,0 @@
-export interface ButtonInterface {
-  buttonText: string;
-}
