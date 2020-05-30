37531989065771bb503959728cf6dd0e812d4c94
d3byte <pandinator@protonmail.com>
2020-05-30 16:50:11 +0300

    feat: initial commit

---
+++ src/app/share/share.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {ReactiveFormsModule} from "@angular/forms";
+import {CoreModule} from "../core/core.module";
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    CoreModule,
+  ]
+})
+export class ShareModule { }
