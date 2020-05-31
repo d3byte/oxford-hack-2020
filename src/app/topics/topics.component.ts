8604271fa1dabbf94314b6332f57b8fa94ca5dfe
Valoriel <kostyan001301@gmail.com>
2020-05-31 12:34:12 +0300

    feat: added topics grid

--- src/app/topics/topics.component.ts
+++ src/app/topics/topics.component.ts
@@ -1,14 +1,84 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+import { TopicInterface } from '@share/interfaces/topic.interface';
+
 @Component({
   selector: 'app-topics',
   templateUrl: './topics.component.html',
   styleUrls: ['./topics.component.scss'],
 })
 export class TopicsComponent implements OnInit {
+  constructor() {}
   public toggleSearch = false;
 
-  constructor() {}
+  public topics: TopicInterface[] = [
+    {
+      title: 'Programming Languages',
+      description: 'A brief description of the topic',
+    },
+    {
+      title: 'Programming Languages',
+    },
+    {
+      title: 'Programming Languages',
+      description: 'A brief description of the topic A brief description of the topic',
+    },
+    {
+      title: 'Programming Languages',
+      description: 'A brief description of the topic',
+    },
+    {
+      title: 'Programming Languages',
+    },
+    {
+      title: 'Programming Languages',
+      description: 'A brief description of the topic',
+    },
+    {
+      title: 'Programming Languages',
+      description: 'A brief description of the topic A brief description of the topic A brief description of the topic A brief description of the topic',
+    },
+    {
+      title: 'Programming Languages',
+    },
+    {
+      title: 'Programming Languages',
+      description: 'A brief description of the topic',
+    },
+    {
+      title: 'Programming Languages',
+      description: 'A brief description of the topic',
+    },
+    {
+      title: 'Programming Languages',
+    },
+    {
+      title: 'Programming Languages',
+      description: 'A brief description of the topic',
+    },
+    {
+      title: 'Programming Languages',
+      description: 'A brief description of the topic',
+    },
+    {
+      title: 'Programming Languages',
+    },
+    {
+      title: 'Programming Languages',
+      description: 'A brief description of the topic A brief description of the topic',
+    },
+    {
+      title: 'Programming Languages',
+      description: 'A brief description of the topic A brief description of the topic A brief description of the topic',
+    },
+    {
+      title: 'Programming Languages',
+    },
+    {
+      title: 'Programming Languages',
+      description: 'A brief description of the topic',
+    },
+  ];
 
   ngOnInit(): void {}
 
